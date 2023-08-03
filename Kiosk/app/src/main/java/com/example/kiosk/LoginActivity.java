258bf2bcaea6aa50310a7c3967be38247515c9a6
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-08-03 21:42:08 +0900

    DB 연결완료

---
+++ Kiosk/app/src/main/java/com/example/kiosk/LoginActivity.java
@@ -0,0 +1,134 @@
+package com.example.kiosk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Button;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String BASE_URL = "http://172.20.10.2:4000/user/login";
+    EditText edt1, edt2;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main);
+        edt1 = findViewById(R.id.id_text);
+        edt2 = findViewById(R.id.password_text);
+        btn1 = findViewById(R.id.login_button);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String name = edt1.getText().toString();
+                String password = edt2.getText().toString();
+
+                // 로그로 아이디와 비밀번호를 출력 (디버그용)
+                Log.d("LoginActivity", "아이디: " + name);
+                Log.d("LoginActivity", "비밀번호: " + password);
+
+                // 서버에 로그인 정보 전송
+                loginToServer(name, password);
+            }
+        });
+    }
+    public void loginToServer(String username, String password) {
+
+        try{
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("id", username);
+            jsonBody.put("password", password);
+            String requestBody = jsonBody.toString();
+            System.out.println(requestBody);
+
+            OkHttpClient client = new OkHttpClient();
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            RequestBody formBody = RequestBody.create(requestBody, JSON);
+
+            Request request = new Request.Builder()
+                    .url(BASE_URL)
+                    .post(formBody)
+                    .build();
+
+            System.out.println("dd");
+//            if(a==1) {
+//                return;
+//            }
+
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("fail");
+                        }
+                    });
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        String responseBody = response.body().string();
+                        try {
+                            JSONObject jsonObject = new JSONObject(responseBody);
+                            boolean ok = jsonObject.getBoolean("ok");
+                            if (ok) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        System.out.println("login success");
+
+                                        // 로그인 성공 시 다음 화면으로 이동하거나 액션을 취할 수 있습니다.
+                                    }
+                                });
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        System.out.println("login fail");
+                                    }
+                                });
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println("fail");
+                            }
+                        });
+                    }
+                }
+            });
+
+
+        } catch(JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+

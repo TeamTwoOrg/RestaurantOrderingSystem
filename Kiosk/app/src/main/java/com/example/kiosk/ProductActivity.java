4f7ea04fb6eabdcef716683d8959fb45c2f88316
8.06_hyogi <101712060+806hyogi@users.noreply.github.com>
2023-08-14 22:55:34 +0900

    에베베베베 승우야 고쳐줘

--- Kiosk/app/src/main/java/com/example/kiosk/ProductActivity.java
+++ Kiosk/app/src/main/java/com/example/kiosk/ProductActivity.java
@@ -10,15 +10,28 @@ import org.json.JSONObject;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SyncStatusObserver;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Space;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +44,7 @@ public class ProductActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_activity);
 
+        new FetchProductDataTask().execute();
         productAll_btn = findViewById(R.id.productAll_btn);
         addButton = findViewById(R.id.addButton);
 
@@ -73,10 +87,180 @@ public class ProductActivity extends AppCompatActivity{
                 }, 200);
                 Intent intent = new Intent(ProductActivity.this, AddMenuPopUp.class);
                 startActivity(intent);
-
-
-
             }
         });
     }
+
+    // 상품보기 리스트 불러오기
+    private class FetchProductDataTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            OkHttpClient client = new OkHttpClient();
+
+            // POST 요청에 필요한 JSON 데이터 생성
+            JSONObject jsonBody = new JSONObject();
+
+
+            try {
+                jsonBody.put("id", LoginActivity.cur_id);
+                jsonBody.put("password", LoginActivity.cur_pw);
+
+
+                // 필요한 다른 데이터도 추가
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            // JSON 데이터를 요청할 데이터로 설정
+            RequestBody requestBody = RequestBody.create(
+                    MediaType.parse("application/json; charset=utf-8"),
+                    jsonBody.toString()
+            );
+
+            Request request = new Request.Builder()
+                    .url("https://sm-kiosk.kro.kr/menu")
+                    .post(requestBody)
+                    .build();
+
+            try {
+
+                Response response = client.newCall(request).execute();
+                return response.body().string();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+
+            if (response != null) {
+                // 서버로부터 받은 데이터를 이용하여 ListView에 표시
+                String[] products = response.split(";"); // 적절한 데이터 형식에 따라 파싱
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                        ProductActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        products
+                );
+//                listView.setAdapter(adapter);
+                parseJSON(response);
+            }
+        }
+
+        protected void parseJSON(String response) {
+            Log.d("test"," dsfdsfsdfdsfsdfff");
+            ArrayList<JSONObject> menuList = new ArrayList<>();
+
+            try {
+                JSONObject responseJson = new JSONObject(response);
+                JSONObject dataJson = responseJson.getJSONObject("data");
+                JSONArray ordersArray = dataJson.getJSONArray("menus");
+
+                for (int i = 0; i < ordersArray.length(); i++) {
+                    JSONObject order = ordersArray.getJSONObject(i);
+                    menuList.add(order);
+                }
+
+                spreadOrderData(menuList);
+//                 테스트 출력 코드
+//                for (JSONObject menuItem : menuList) {
+//                    // JSONObject를 보기 좋게 로그에 출력
+//                    String jsonMenuItem = menuItem.toString(4); // 들여쓰기 4칸으로 출력
+//                    Log.d("test", jsonMenuItem);
+////                                System.out.println(menuItem.getString("imageURL"));
+//                }
+
+            } catch (JSONException e) {
+//                            System.err.println("실패");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void spreadOrderData(ArrayList<JSONObject> menuList) throws JSONException {
+        RelativeLayout allOrderView = findViewById(R.id.switching_zone);
+        allOrderView.removeAllViews();
+
+        Typeface customFont = ResourcesCompat.getFont(this, R.font.gmarketsanslight);
+
+        for(int i=0; i<menuList.size(); i++) {
+            RelativeLayout newLayout = new RelativeLayout(this);
+            newLayout.setLayoutParams(new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            ));
+            newLayout.setBackgroundResource(R.drawable.bottom_border);
+
+            // 상품 이미지 (Glide 사용)
+            ImageView productImage = new ImageView(this);
+            productImage.setId(View.generateViewId());
+            productImage.setLayoutParams(new RelativeLayout.LayoutParams(
+                    400, // 이미지 너비를 원하는 값으로 설정
+                    400  // 이미지 높이를 원하는 값으로 설정
+            ));
+            Glide.with(this)
+                    .load(menuList.get(i).getString("imageURL"))
+                    .into(productImage);
+            newLayout.addView(productImage);
+
+            // 이름, 가격, 추가 설명
+            RelativeLayout textLayout = new RelativeLayout(this);
+            RelativeLayout.LayoutParams textLayoutParams = new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            textLayoutParams.addRule(RelativeLayout.RIGHT_OF, productImage.getId()); // 이미지 오른쪽에 배치
+            textLayout.setLayoutParams(textLayoutParams);
+
+            int textTopPadding = 20; // 각 텍스트 사이의 간격을 조절하는 값
+
+            // 이름
+            TextView nameText = new TextView(this);
+            nameText.setId(View.generateViewId());
+            nameText.setLayoutParams(new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            ));
+            nameText.setTextSize(28);
+            nameText.setTextColor(Color.WHITE);
+            nameText.setTypeface(customFont);
+            nameText.setPadding(0, textTopPadding, 0, 0); // 상단 패딩 추가
+            nameText.setText("상품 이름: " + menuList.get(i).getString("name"));
+            textLayout.addView(nameText);
+
+            // 가격
+            TextView priceText = new TextView(this);
+            priceText.setLayoutParams(new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            ));
+            priceText.setTextSize(28);
+            priceText.setTextColor(Color.WHITE);
+            priceText.setTypeface(customFont);
+            priceText.setPadding(0, textTopPadding, 0, 0); // 상단 패딩 추가
+            priceText.setText("상품 가격: " + menuList.get(i).getString("price"));
+            textLayout.addView(priceText);
+
+            // 추가 설명
+            TextView subText = new TextView(this);
+            subText.setLayoutParams(new RelativeLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            ));
+            subText.setTextSize(28);
+            subText.setTextColor(Color.WHITE);
+            subText.setTypeface(customFont);
+            subText.setPadding(0, textTopPadding, 0, 0); // 상단 패딩 추가
+            subText.setText("상품 설명: " + menuList.get(i).getString("text"));
+            textLayout.addView(subText);
+
+            newLayout.addView(textLayout);
+
+            allOrderView.addView(newLayout);
+        }
+    }
+
 }
